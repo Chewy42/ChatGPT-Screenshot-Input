a46532848a4b0b71227a460874f3918dd8223a01
Conor Wong <90535796+JiaxiongWeng-Conor@users.noreply.github.com>
2023-04-02 13:57:12 -0400

    Update popup.js

--- main/popup.js
+++ main/popup.js
@@ -2,7 +2,7 @@ document.getElementById("capture").addEventListener("click", () => {
   chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
     // 在这里调用OCR API来识别文本
     const ocrApiUrl = "https://vision.googleapis.com/v1/images:annotate";
-    const apiKey = "AIzaSyA7DgNWwtUPQ6iEFuvNkFLtnZ3ZPoGtI_k";
+    const apiKey = "AIzaSyD1eOkchyfZu1BZ2gtIk1R7aph6m4_cPjA";
     const requestData = {
       url: ocrApiUrl,
       method: "POST",
