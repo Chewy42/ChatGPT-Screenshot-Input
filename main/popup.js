45b5a87092890d8f5eff03ecf6a13cbcb6ae6096
Conor Weng <90535796+JiaxiongWeng-Conor@users.noreply.github.com>
2023-04-01 21:17:37 -0400

    Update popup.js

--- main/popup.js
+++ main/popup.js
@@ -1,5 +1,29 @@
-document.getElementById('capture').addEventListener('click', () => {
-  chrome.runtime.sendMessage({action: 'captureText'}, (response) => {
-    document.getElementById('recognizedText').value = response.recognizedText;
+document.getElementById("capture").addEventListener("click", () => {
+  chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
+    // 在这里调用OCR API来识别文本
+    const ocrApiUrl = "https://your-ocr-api-url";
+    const apiKey = "your-api-key";
+    const requestData = {
+      url: ocrApiUrl,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "apikey": apiKey,
+      },
+      body: JSON.stringify({ base64Image: dataUrl.split(",")[1] }),
+    };
+    fetch(requestData)
+      .then((response) => response.json())
+      .then((ocrResult) => {
+        const extractedText = ocrResult["ParsedResults"][0]["ParsedText"];
+        // 将提取到的文本插入到ChatGPT输入框
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          const code = `
+            document.querySelector('.input-container textarea').value = \`${extractedText}\`;
+          `;
+          chrome.tabs.executeScript(tabs[0].id, { code });
+        });
+      })
+      .catch((error) => console.error("Error:", error));
   });
 });
